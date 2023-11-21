1d5008e178ba01647ba338c14a2a7a0ed72757ed
Oli <oliviercillo@gmail.com>
2023-11-21 21:31:18 +0100

    Misc cleaning

--- src/settings.js
+++ src/settings.js
@@ -2,9 +2,7 @@ import { useRef } from 'react'
 import { useControls, folder, monitor } from 'leva'
 
 export const SETTINGS = {
-  MODE: ['Images', 'Typography'],
-  DIRECTIONS: ['Horizontal', 'Vertical'],
-  MAX_INSTANCES: 8
+
 }
 
 export function useLevaControls (props) {
@@ -12,32 +10,12 @@ export function useLevaControls (props) {
 
   const data = useControls({
     'Main Settings': folder({
-      Mode: {
-        options: SETTINGS.MODE
-      },
-      Direction: {
-        options: SETTINGS.DIRECTIONS
-      },
-      Instances: {
-        value: 4,
+      Divisions: {
+        value: 12,
         min: 2,
-        max: SETTINGS.MAX_INSTANCES,
+        max: 96,
         step: 1
       },
-      Resolution: folder({
-        Divisions: {
-          value: 12,
-          min: 2,
-          max: 96,
-          step: 1
-        },
-        Resolution: {
-          value: 100,
-          min: 1,
-          max: 100,
-          step: 5
-        }
-      }),
       'Debug Zone': folder({
         framerateMonitor: monitor(frameRateRef, { graph: true, interval: 60 }),
         framerate: monitor(frameRateRef, { graph: false, interval: 60 })
--- src/sketch/mainSketch.js
+++ src/sketch/mainSketch.js
@@ -1,16 +1,20 @@
+import ShaderManager from './shaderManager'
+import PostProcessor from './postProcessor'
 import blurH from './shaders/blurH.frag'
 import blurV from './shaders/blurV.frag'
 import colorama from './shaders/colorama.frag'
 import pixel from './shaders/pixel.frag'
 import pixelV from './shaders/pixelV.frag'
 import posterize from './shaders/posterize.frag'
-import ShaderManager from './shaderManager'
-import PostProcessor from './postProcessor'
+
+import imageSrc from './../assets/1.png'
 
 export const mainSketch = ({ canvasRef, settingsRef }) => (s) => {
   let shaderManager, postProcessor
   const blurIterations = 21
   let coloramaTime = 0
+
+  let imageL
 
   s.setup = () => {
     const canvas = s.createCanvas(500, 500, s.WEBGL)
@@ -28,6 +32,8 @@ export const mainSketch = ({ canvasRef, settingsRef }) => (s) => {
     shaderManager.loadShader('pixel', pixel)
     shaderManager.loadShader('pixelV', pixelV)
     shaderManager.loadShader('posterize', posterize)
+
+    imageL = s.loadImage(imageSrc)
   }
 
   s.draw = () => {
@@ -46,14 +52,14 @@ export const mainSketch = ({ canvasRef, settingsRef }) => (s) => {
     }
 
     // COLORAMA
-    postProcessor.applyShader('colorama', { time: coloramaTime, vColor1: [1.0, 1.0, 1.0], vColor2: [0.0, 0.0, 0.0], gradientLoops: 1 })
+    postProcessor.applyShader('colorama', { time: coloramaTime, vColor1: [1.0, 1.0, 1.0], vColor2: [0.0, 0.0, 0.0], gradientLoops: 3 })
     coloramaTime += 0.01
 
     // PIXEL
     postProcessor.applyShader('pixel', { resolution: [s.width, s.height], pixelSize: 5 })
 
     // POSTERIZE
-    // postProcessor.applyShader('posterize', {levels: 3});
+    postProcessor.applyShader('posterize', { levels: 3 })
 
     // VERTICAL PIXEL
     postProcessor.applyShader('pixelV', { resolution: s.height, divisions: settingsRef.current.Divisions })
